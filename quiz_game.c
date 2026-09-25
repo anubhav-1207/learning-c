9c81ad6d1e23233fe82be47987efbe9f935d25f8
anubhav-1207 <anubhabyt123@gmail.com>
2026-09-25 23:18:55 +0530

    Refactor quiz game code for improved readability and consistency

--- quiz_game.c
+++ quiz_game.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 
-int main(){
-    char ques[][100] = { {"Largest planet?"},
-                        {"Biggest continent?"},
-                        {"Smallest country? "}
-                    };
-    
-    char options[][100] = {
-                        {"A.Saturn\nB.Uranus\nC.Jupiter\nD.Earth"},
-                        {"A.Asia\nB.Africa\nC.Russia\nD.Australia"},
-                        {"A.China\nB.India\nC.Vatican City\nD.Sri Lanka"}
-    };
+int main() {
+  char ques[][100] = {
+      {"Largest planet?"}, {"Biggest continent?"}, {"Smallest country? "}};
 
-    char answerKey[] = {'C','A','C'};
+  char options[][100] = {{"A.Saturn\nB.Uranus\nC.Jupiter\nD.Earth"},
+                         {"A.Asia\nB.Africa\nC.Russia\nD.Australia"},
+                         {"A.China\nB.India\nC.Vatican City\nD.Sri Lanka"}};
 
-    char answer;
-    int correct = 0;
+  char answerKey[] = {'C', 'A', 'C'};
 
-    for(int i = 0; i < 3; i++){
-        printf("%s\n",ques[i]);
-        printf("%s\n\n",options[i]);
-        scanf(" %c",&answer);
+  char answer;
+  int correct = 0;
 
-        if (answer == answerKey[i]){
-            correct += 1;
-            continue;
-        }
+  for (int i = 0; i < 3; i++) {
+    printf("%s\n", ques[i]);
+    printf("%s\n\n", options[i]);
+    scanf(" %c", &answer);
+
+    if (answer == answerKey[i]) {
+      correct += 1;
+      continue;
     }
+  }
 
-    printf("Congratulations! You got %d correct out of 4.\n",correct);
+  printf("Congratulations! You got %d correct out of 4.\n", correct);
 }
